d19f6f842bd610ca099fc07920ad7caa5f1d9749
Andreas Gade Jensen <andreasgadej@gmail.com>
2020-01-17 12:58:36 +0100

    test changes

--- resources/js/tool.js
+++ resources/js/tool.js
@@ -1,9 +1,10 @@
 Nova.booting((Vue, router, store) => {
     router.addRoutes([
         {
-            name: 'settings-tool',
-            path: '/settings',
-            component: require('./SettingsTool'),
-        },
-    ])
-})
+            name: "settings-tool",
+            path: "/settings",
+            component: require("./SettingsTool")
+        }
+    ]);
+    console.log("hejs hejsa");
+});
